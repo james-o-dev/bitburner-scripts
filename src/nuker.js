bf42d914a26a8e290df720ec6e37996883a36b8e
jameso <james.o.dev@gmail.com>
2021-12-20 16:16:57 +1300

    refactor(nuker): Add the rest of the tools; Exclusion list

--- src/nuker.js
+++ src/nuker.js
@@ -1,19 +1,28 @@
+// Add any servers to exclude from nuking.
+const exclusions = [
+  'darkweb',
+  'home',
+]
+
 /** @param {NS} ns **/
 export async function main(ns) {
   const { servers, tools } = JSON.parse(await ns.read('config.txt'))
 
-const nukable = servers.filter(s => s.canHack && !s.hasRootAccess)
-ns.tprint('Nukable: ' + nukable.length)
+  const nukable = servers.filter(s => s.canHack && !s.hasRootAccess && !exclusions.includes(s.name))
+  ns.tprint('Nukable: ' + nukable.length)
 
-for(let server of nukable) {
-  const serverName = server.name
-  if (tools.includes('BruteSSH.exe')) ns.brutessh(serverName)
-  if (tools.includes('FTPCrack.exe')) ns.ftpcrack(serverName)
+  for (let server of nukable) {
+    const serverName = server.name
+    if (tools.includes('BruteSSH.exe')) ns.brutessh(serverName)
+    if (tools.includes('FTPCrack.exe')) ns.ftpcrack(serverName)
+    if (tools.includes('HTTPWorm.exe')) ns.httpworm(serverName)
+    if (tools.includes('relaySMTP.exe')) ns.relaysmtp(serverName)
+    if (tools.includes('SQLInject.exe')) ns.sqlinject(serverName)
 
-  ns.nuke(serverName)
-  ns.tprint('Nuked: ' + serverName)
-}
+    ns.nuke(serverName)
+    ns.tprint('Nuked: ' + serverName)
+  }
 
-// Re-run init.js
-ns.run('init.js')
+  // Re-run init.js
+  ns.run('init.js')
 }
