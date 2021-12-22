e64fc8c04473bb87b189d0a608a4250acda625f1
jameso <james.o.dev@gmail.com>
2021-12-22 15:58:28 +1300

    refactor(purchase-servers): Update, add flag options
    * `--dry-run`: Do not purchase servers, only print out the first/max server
    * `--limit n`: Purchase up to n servers; Default n=1
    * Message formatting

--- src/purchase-servers.js
+++ src/purchase-servers.js
@@ -1,14 +1,31 @@
+const flagsConfig = [
+  ['dry-run', false],
+  ['limit', 1]
+]
+
 /** @param {NS} ns **/
 export async function main(ns) {
+  const flags = ns.flags(flagsConfig)
+  const limit = flags.limit
+
   const maxLimit = ns.getPurchasedServerLimit() - ns.getPurchasedServers().length
 
   for (let i = 0; i < maxLimit; i++) {
-      const ram = getMaxAffordable(ns)
-      if (ram) {
-          const cost = ns.getPurchasedServerCost(ram)
-          const server = ns.purchaseServer(`pserv${Math.random()}`, ram)
-          ns.tprint(`Purchased ${server} | ${ns.nFormat(ram, '0,0')} | ${ns.nFormat(cost, '0,0')}`)
+    if (i > limit - 1) break
+
+    const ram = getMaxAffordable(ns)
+    if (ram) {
+      const cost = ns.getPurchasedServerCost(ram)
+      const ramf = `${ns.nFormat(ram, '0,0')}GB`
+
+      if (flags['dry-run']) {
+        ns.tprint(`DRY-RUN: MAX ${ramf} | COST ${ns.nFormat(cost, '$0,0.00')}`)
+        return
       }
+
+      const server = ns.purchaseServer(`pserv${Math.random()}`, ram)
+      ns.tprint(`Purchased ${server} | ${ramf} | ${ns.nFormat(cost, '$0,0.00')}`)
+    }
   }
 }
 
@@ -17,11 +34,11 @@ const getMaxAffordable = (ns) => {
   const money = ns.getPlayer().money
 
   for (let i = 20; i >= 0; i--) {
-      const ram = Math.pow(2, i)
-      const cost = ns.getPurchasedServerCost(ram)
+    const ram = Math.pow(2, i)
+    const cost = ns.getPurchasedServerCost(ram)
 
-      if (cost <= money) {
-          return ram
-      }
+    if (cost <= money) {
+      return ram
+    }
   }
 }
