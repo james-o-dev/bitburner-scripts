7a1fa1bc9eb27bc90594b91a8d0c2a7f44cb8b4c
jameso <james.o.dev@gmail.com>
2021-12-26 11:17:54 +1300

    feat(run): Another rework
    * WG and WGWH is now GW and HWGW respectively
    * "Batches" are now concurrent
    * Update handling of polling
    * Attempt to ensure that all threads of the batch can fit into our available server threads
    * todo: "Auto-correct" threads?

--- src/run.js
+++ src/run.js
@@ -1,4 +1,4 @@
-import { GAME_CONSTANTS, getScriptRam, getScriptTime, getServers, killall, SCRIPT, SETTINGS, stringify } from 'shared.js'
+import { GAME_CONSTANTS, getScriptRam, getScriptTime, getServers, SCRIPT, SETTINGS, stringify } from 'shared.js'
 
 /** @param {NS} ns **/
 export async function main(ns) {
@@ -9,209 +9,187 @@ export async function main(ns) {
     target = target[0]
     if (!target) throw new Error('Please specify the target server name as the argument.')
 
+    if (SETTINGS.MONEY_SAFETY_TRESH >= SETTINGS.MONEY_THRESH) throw new Error('MONEY_SAFETY_TRESH must be below MONEY_THRESH.')
+
     const usable = getServers(ns).filter(s => s.hasRootAccess && s.maxRam > 0).sort((a, b) => b.maxRam - a.maxRam)
 
-    let lastKnown = {
-        moneyAvailable: ns.getServerMoneyAvailable(target.name),
-        securityLevel: ns.getServerSecurityLevel(target.name),
-        timestamp: 0,
-    }
-
-    while (true) {
-        // WG until max money and min security, then hack via WGWH.
-        const wgLoopReturn = await wgLoop(ns, target, lastKnown, usable)
-        killall(ns)
-        lastKnown = wgLoopReturn.lastKnown
-        lastKnown.moneyAvailable = target.maxMoney
-        lastKnown.securityLevel = target.minSecurityLevel
-
-        // WGWH to hack, until it needs to re-grow the server via WG.
-        const wgwhLoopReturn = await wgwhLoop(ns, target, lastKnown, usable)
-        killall(ns)
-        lastKnown = wgwhLoopReturn.lastKnown
-        lastKnown.moneyAvailable = ns.getServerMoneyAvailable(target.name)
-        lastKnown.securityLevel = ns.getServerSecurityLevel(target.name)
-    }
+    // GW until max money and min security, then hack via HWGW.
+    ns.tprint('GW started.')
+    await gwLoop(ns, target, usable)
+    ns.tprint('GW finished; HWGW started.')
+    await hwgwLoop(ns, target, usable)
 }
 
 /** @param {NS} ns **/
-const wgLoop = async (ns, target, lastKnown, usable) => {
+const gwLoop = async (ns, target, usable) => {
 
-    const startMessage = getStartPrint(ns, target, false)
-
-    // GW until max money and min security.
-    const wg = [0, 0]
-
+    let poll = 0
     while (true) {
+        await ns.sleep(poll)
+        poll = SETTINGS.POLL
+
         const moneyAvailable = ns.getServerMoneyAvailable(target.name)
-        lastKnown.moneyAvailable = moneyAvailable
         const securityLevel = ns.getServerSecurityLevel(target.name)
 
-        // Finished with WG.
-        if (moneyAvailable === target.maxMoney && securityLevel <= target.minSecurityLevel) return { lastKnown }
+        if (securityLevel <= target.minSecurityLevel && moneyAvailable === target.maxMoney) break
 
-        await ns.sleep(SETTINGS.POLL)
-
-        if (wg[0] <= 0 && wg[1] <= 0) {
-            wg[0] = getReqWeakenThreads(lastKnown, target)
-            lastKnown.securityLevel = target.minSecurityLevel
-
-            wg[1] = getReqGrowThreads(lastKnown, target, ns)
-            lastKnown.securityLevel += ns.growthAnalyzeSecurity(wg[1])
+        let exec = []
+        if (moneyAvailable < target.maxMoney) {
+            exec.push({
+                script: SCRIPT.GROW,
+                scriptRam: getScriptRam(SCRIPT.GROW),
+                scriptTime: getScriptTime(ns, SCRIPT.GROW, target.name),
+                threads: getReqGrowThreads(target, ns, moneyAvailable)
+            })
+        }
+        if (securityLevel > target.minSecurityLevel) {
+            exec.push({
+                script: SCRIPT.WEAKEN,
+                scriptRam: getScriptRam(SCRIPT.WEAKEN),
+                scriptTime: getScriptTime(ns, SCRIPT.WEAKEN, target.name),
+                threads: getReqWeakenThreads(target, target),
+            })
         }
 
-        let script = ''
-        let wgIdx = -1
-        if (wg[0] > 0) {
-            script = SCRIPT.WEAKEN
-            wgIdx = 0
-        } else if (wg[1] > 0) {
-            script = SCRIPT.GROW
-            wgIdx = 1
-        } else {
-            continue
-        }
+        const polls = getPollDifferences(exec.map(e => e.scriptTime))
 
-        const exec = execOnServers(ns, usable, target, script, wg[wgIdx], lastKnown.timestamp)
-        lastKnown.timestamp = exec.scriptEndTimestamp
-        wg[wgIdx] -= exec.threadsUsed
+        exec = exec.map((e, i) => {
+            e.poll = polls[i]
+            return e
+        })
+
+        for (const queued of exec) {
+            for (const server of usable) {
+                if (queued.threads <= 0) break
+
+                const threads = getScriptServerThreads(ns, server, queued.threads, queued.scriptRam)
+
+                if (threads > 0) {
+                    ns.exec(queued.script, server.name, threads, target.name, queued.poll, Math.random())
+                    queued.threads -= threads
+                }
+            }
+        }
 
         ns.clearLog()
-        ns.print('WG')
-				const timeToStart = startMessage.startTime - Date.now()
-				if (timeToStart > 0) ns.print(ns.tFormat(timeToStart))
+        ns.print('GW')
         printStatus(ns, target)
-        ns.print(stringify(wg))
     }
 }
 
 /** @param {NS} ns **/
-const wgwhLoop = async (ns, target, lastKnown, usable) => {
+const hwgwLoop = async (ns, target, usable) => {
+    const safetyMoney = target.maxMoney * SETTINGS.MONEY_SAFETY_TRESH
 
-    const startMessage = getStartPrint(ns, target, true)
-
-    // Do WGWH cycle.
-    const wgwh = [0, 0, 0, 0]
-
-		let initialHackThreads = 0
+    let initialHackThreads = 0
+    let cycleEndTimestamp = 0
     while (true) {
         await ns.sleep(SETTINGS.POLL)
 
-        if (SETTINGS.MONEY_SAFETY_TRESH >= SETTINGS.MONEY_THRESH) throw new Error('MONEY_SAFETY_TRESH must be below MONEY_THRESH.')
         const moneyAvailable = ns.getServerMoneyAvailable(target.name)
-        const safetyMoney = target.maxMoney * SETTINGS.MONEY_SAFETY_TRESH
 
-        // Needs to re-grow via WG
+        // Kill all if it currently goes below the safety money threshold.
         if (moneyAvailable < safetyMoney) {
-            // ns.run('killall.js')
-            // throw new Error(`Stopped: Went below ${SETTINGS.MONEY_SAFETY_TRESH} money threshold.`)
-            return { lastKnown }
+            ns.run('killall.js')
+            throw new Error(`Stopped: Went below ${SETTINGS.MONEY_SAFETY_TRESH} money threshold.`)
         }
 
-        if (wgwh[0] <= 0 && wgwh[1] <= 0 && wgwh[2] <= 0 && wgwh[3] <= 0) {
-            wgwh[0] = getReqWeakenThreads(lastKnown, target)
-            lastKnown.securityLevel = target.minSecurityLevel
+        const hwgw = [
+            { script: SCRIPT.HACK, scriptRam: getScriptRam(SCRIPT.HACK), scriptTime: getScriptTime(ns, SCRIPT.HACK, target.name), threads: 0 },
+            { script: SCRIPT.WEAKEN, scriptRam: getScriptRam(SCRIPT.WEAKEN), scriptTime: getScriptTime(ns, SCRIPT.WEAKEN, target.name), threads: 0 },
+            { script: SCRIPT.GROW, scriptRam: getScriptRam(SCRIPT.GROW), scriptTime: getScriptTime(ns, SCRIPT.GROW, target.name), threads: 0 },
+            { script: SCRIPT.WEAKEN, scriptRam: getScriptRam(SCRIPT.WEAKEN), scriptTime: getScriptTime(ns, SCRIPT.WEAKEN, target.name), threads: 0 }
+        ]
 
-            wgwh[1] = getReqGrowThreads(lastKnown, target, ns)
-            lastKnown.securityLevel += ns.growthAnalyzeSecurity(wgwh[1])
+        // Determine polling.
+        const polls = getPollDifferences(hwgw.map(h => h.scriptTime))
+				polls[polls.length - 1] += SETTINGS.POLL
 
-            wgwh[2] = getReqWeakenThreads(lastKnown, target)
-            lastKnown.securityLevel = target.minSecurityLevel
+        // First determine the money threshold so that all our servers can currently accomodate an entire batch.
+        let moneyThresh = SETTINGS.MONEY_THRESH
+        let exec = []
+        while (moneyThresh < 1 && exec.length === 0) {
+            let h0Threads = getReqHackThreads(target, moneyThresh, ns)
+						if (!initialHackThreads) initialHackThreads = h0Threads
+						else if (h0Threads > initialHackThreads) h0Threads = initialHackThreads
 
-            wgwh[3] = getReqHackThreads(target, ns)
-            lastKnown.securityLevel += ns.hackAnalyzeSecurity(wgwh[3])
-            lastKnown.moneyAvailable = target.maxMoney * SETTINGS.MONEY_THRESH
+            const w1Threads = getReqWeakenThreads(target, target.minSecurityLevel + ns.hackAnalyzeSecurity(h0Threads))
+            const g2Threads = getReqGrowThreads(target, ns, target.maxMoney * moneyThresh)
+            const w3Threads = getReqWeakenThreads(target, target.minSecurityLevel + ns.growthAnalyzeSecurity(g2Threads))
 
-						// Only use up to the initial hack thread number.
-						// This assumes that the WGWH cycle initially started with max money and min security.
-						if (!initialHackThreads) initialHackThreads = wgwh[3]
-						else if (wgwh[3] > initialHackThreads) wgwh[3] = initialHackThreads
+            const reqThreads = [h0Threads, w1Threads, g2Threads, w3Threads]
+            const workingExec = []
+            for (let i = 0; i < hwgw.length; i++) {
+                const script = hwgw[i]
+
+                for (const server of usable) {
+                    if (reqThreads[i] <= 0) break
+
+                    const threads = getScriptServerThreads(ns, server, reqThreads[i], script.scriptRam)
+
+                    if (threads > 0) {
+                        workingExec.push({
+                            script: script.script,
+                            server: server.name,
+                            threads,
+                            target: target.name,
+                            poll: polls[i]
+                        })
+                        reqThreads[i] -= threads
+                    }
+                }
+            }
+
+            // There are threads required that are left over; Increase the money threshold and retry.
+            if (reqThreads.find(rq => rq > 0)) {
+                moneyThresh += 0.01
+            }
+            // All threads of the batch is able to fit.
+            else {
+                exec = workingExec
+            }
         }
 
-        let script = ''
-        let wgwhIdx = -1
-        if (wgwh[0] > 0) {
-            script = SCRIPT.WEAKEN
-            wgwhIdx = 0
-        } else if (wgwh[1] > 0) {
-            script = SCRIPT.GROW
-            wgwhIdx = 1
-        } else if (wgwh[2] > 0) {
-            script = SCRIPT.WEAKEN
-            wgwhIdx = 2
-        } else if (wgwh[3] > 0) {
-            script = SCRIPT.HACK
-            wgwhIdx = 3
-        } else {
-            continue
+        // Then execute them on the servers
+        if (exec.length > 0) {
+            for (const queued of exec) {
+								ns.exec(queued.script, queued.server, queued.threads, queued.target, queued.poll, Math.random())
+            }
         }
-
-        const exec = execOnServers(ns, usable, target, script, wgwh[wgwhIdx], lastKnown.timestamp)
-        lastKnown.timestamp = exec.scriptEndTimestamp
-        wgwh[wgwhIdx] -= exec.threadsUsed
 
         ns.clearLog()
-        ns.print('WGWH')
-				const timeToStart = startMessage.startTime - Date.now()
-				if (timeToStart > 0) ns.print(ns.tFormat(timeToStart))
+        ns.print('HWGW')
         printStatus(ns, target)
-        ns.print(stringify(wgwh))
     }
 }
 
 /** @param {NS} ns **/
-const getReqGrowThreads = (lastKnown, target, ns) => {
-    const growDiffPct = target.maxMoney / lastKnown.moneyAvailable
+const getReqGrowThreads = (target, ns, moneyAvailable) => {
+    const growDiffPct = target.maxMoney / moneyAvailable
     return Math.ceil(ns.growthAnalyze(target.name, growDiffPct))
 }
 
-/** @param {NS} ns **/
-const getReqHackThreads = (target, ns) => {
-    const moneyMinimum = target.maxMoney * SETTINGS.MONEY_THRESH
+/**
+ *
+ * @param {*} target
+ * @param {*} moneyThresh If the batch takes too much threads, increase this;
+ * - Should stop creating batches if this goes over a certain threshold, to avoid creating 1-threaded hack batches
+ * @param {NS} ns
+ * @returns
+ */
+const getReqHackThreads = (target, moneyThresh, ns) => {
+    const moneyMinimum = target.maxMoney * moneyThresh
     const hackDiff = target.maxMoney - moneyMinimum
     return Math.floor(ns.hackAnalyzeThreads(target.name, hackDiff))
 }
 
-const getReqWeakenThreads = (lastKnown, target) => {
-    const weakenDiff = lastKnown.securityLevel - target.minSecurityLevel
+const getReqWeakenThreads = (target, securityLevel) => {
+    const weakenDiff = securityLevel - target.minSecurityLevel
     return Math.ceil(weakenDiff / GAME_CONSTANTS.WEAKEN_THREAD_ANALYZE)
 }
 
 /** @param {NS} ns **/
-const execOnServers = (ns, usableServers, target, script, reqThreads, lastTimestamp) => {
-    const scriptRam = getScriptRam(script)
-
-    let scriptEndTimestamp = Date.now() + getScriptTime(ns, script, target.name)
-    let scriptPoll = 0
-    let threadsUsed = 0
-
-    // If it will finish before the last timestamp: add extra poll time so that it finishes after, in order to maintain the order.
-    if (scriptEndTimestamp < lastTimestamp) scriptPoll = lastTimestamp - scriptEndTimestamp
-
-    scriptPoll += SETTINGS.POLL
-    scriptEndTimestamp += scriptPoll
-
-    // Check available servers.
-    for (let server of usableServers) {
-        if (reqThreads <= 0) break
-
-        const threads = getUsableServerThreads(ns, server, reqThreads, scriptRam)
-
-        if (threads > 0) {
-            ns.exec(script, server.name, threads, target.name, scriptPoll, Math.random())
-            reqThreads -= threads
-            threadsUsed += threads
-        }
-    }
-
-    return {
-        scriptEndTimestamp: scriptEndTimestamp,
-        threadsUsed,
-    }
-}
-
-/** @param {NS} ns **/
-const getUsableServerThreads = (ns, server, threadsReq, scriptRam) => {
+const getScriptServerThreads = (ns, server, threadsReq, scriptRam) => {
     if (threadsReq <= 0) return 0
 
     const threadsAvailable = Math.floor((server.maxRam - ns.getServerUsedRam(server.name)) / scriptRam)
@@ -233,23 +211,7 @@ const printStatus = (ns, target) => {
     ns.print(`MONEY: ${ns.nFormat(moneyAvailable, nFormat)} / ${ns.nFormat(target.maxMoney, nFormat)} (${Math.round((moneyAvailable / target.maxMoney) * 100)}%)`)
 }
 
-/** @param {NS} ns **/
-const getStartPrint = (ns, target, includeHack = false) => {
-    const startPoll = Math.max(
-        getScriptTime(ns, SCRIPT.GROW, target.name),
-        getScriptTime(ns, SCRIPT.WEAKEN, target.name),
-				includeHack ? getScriptTime(ns, SCRIPT.HACK, target.name) : 0
-    )
-		const startTime = Date.now() + startPoll + SETTINGS.POLL
-    const startTimestamp = new Date(Date.now() + startPoll + SETTINGS.POLL).toLocaleTimeString()
-
-    return {
-			message: `Start: ${new Date(startTimestamp).toLocaleTimeString()}`,
-			startTime,
-		}
+const getPollDifferences = (polls) => {
+    const maxPoll = polls.reduce((m, p) => m < p ? p : m, 0)
+    return polls.map((p, i) => maxPoll - p + (i * SETTINGS.POLL))
 }
-
-// const getPollDifferences = (polls) => {
-//     const maxPoll = polls.reduce((m, p) => m < p ? p : m, 0)
-//     return polls.map((p, i) => maxPoll - p + (i * SETTINGS.POLL))
-// }
