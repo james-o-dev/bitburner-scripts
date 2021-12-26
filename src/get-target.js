7295e592193819e877836e06a81e15ce07c489d4
jameso <james.o.dev@gmail.com>
2021-12-26 22:37:39 +1300

    feat(get-target): Updates to script
    * Takes arguments
      * first: number of top targets to return
      * second: true to take into account script times
    * Return script times, use Formulas.exe if they exist

--- src/get-target.js
+++ src/get-target.js
@@ -3,15 +3,17 @@ import { GAME_CONSTANTS, getServers, stringify } from 'shared.js'
 /** @param {NS} ns **/
 export async function main(ns) {
     // ns.disableLog('ALL')
+    const n = ns.args[0] || 1
+		const includeTiming = ns.args[1] || false
 
-		const excludeTargets = [
-			'n00dles'
-		]
+    const excludeTargets = [
+        'n00dles'
+    ]
 
     // Get the profitable server, based on the above metric.
     const target = getServers(ns)
         .filter(t => {
-						if (excludeTargets.includes(t.name)) return false
+            if (excludeTargets.includes(t.name)) return false
             if (t.maxMoney <= 0) return false
             if (!t.hasRootAccess) return false
             if (t.name === GAME_CONSTANTS.HOME) return false
@@ -21,16 +23,31 @@ export async function main(ns) {
             return true
         })
         .map(t => {
+						let growTime = ns.getGrowTime(t.name)
+						let hackTime = ns.getHackTime(t.name)
+						let weakenTime = ns.getWeakenTime(t.name)
+
+						if (ns.fileExists('Formulas.exe')) {
+							const server = ns.getServer(t.name)
+							const player = ns.getPlayer()
+							growTime = ns.formulas.hacking.growTime(server, player)
+							hackTime = ns.formulas.hacking.hackTime(server, player)
+							weakenTime = ns.formulas.hacking.weakenTime(server, player)
+						}
+
             let value = t.maxMoney * ns.hackAnalyzeChance(t.name)
-            // value =  value * t.serverGrowth
+            value = value * t.serverGrowth
             value = value / t.minSecurityLevel
-						// Uncomment below if you are testing and do not want to wait a long time for the scripts to start.
-            // value = value / (ns.getGrowTime(t.name) + ns.getHackTime(t.name) + ns.getWeakenTime(t.name)) // For testing
+						if (includeTiming) value = value / (growTime + hackTime + weakenTime) // For mainly testing.
 
             t.value = value
+						t.hackTime = ns.tFormat(hackTime)
+						t.growTime = ns.tFormat(growTime)
+						t.weakenTime = ns.tFormat(weakenTime)
             return t
         })
         .sort((a, b) => b.value - a.value)
 
-    ns.tprint(stringify(target[0]))
+    const printMe = target.splice(0, n)
+    ns.tprint(stringify(printMe))
 }
